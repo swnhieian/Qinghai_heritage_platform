a0e916f6dc8d4b666eb7d8a244f6fdb009aaa947
Shi Weinan <swnhieian@126.com>
2018-07-25 19:53:05 +0800

    add three model

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TopBar from './TopBar';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Home';
 import Catalog from './Catalog';
-// import About from './About';
+import About from './About';
 import Detail from './Detail';
 import Footer from './Footer';
 import SearchResults from './SearchResults';
@@ -35,9 +35,9 @@ class App extends Component {
           <Route path="/detail/:title" component={Detail}></Route>
           <Route path="/search" component={SearchResults}></Route>
           <Route path="/discovery" component={Discovery}></Route>
+          <Route path="/about" component={About}></Route>
           <Route component={Page404}></Route>
          </Switch>
-          {/*<Route path="/about" component={About}></Route> */}
         </div>
         <Footer></Footer>
       </div>
