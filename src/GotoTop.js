007ebf01eb097081646bcf0c345cb831063f28fa
Shi Weinan <swnhieian@126.com>
2018-08-02 12:07:49 +0800

    modify gotoTop button

--- src/GotoTop.js
+++ src/GotoTop.js
@@ -1,38 +1,18 @@
 import React, { Component } from 'react';
 import './dist/style.css';
-import {Button} from 'reactstrap';
-import { Icon } from 'react-icons-kit';
-import {chevronUp} from 'react-icons-kit/fa/chevronUp';
+import {CircleArrow as ScrollUpButton} from "react-scroll-up-button";
 
 class GotoTop extends Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            intervalId: 0
-        };
-    }
-    
-    scrollStep() {
-        if (window.pageYOffset === 0) {
-            clearInterval(this.state.intervalId);
-        }
-        window.scroll(0, window.pageYOffset - this.props.scrollStepInPx);
-    }
-    
-    scrollToTop() {
-        let intervalId = setInterval(this.scrollStep.bind(this), this.props.delayInMs);
-        this.setState({ intervalId: intervalId });
-    }
     
     render () {
         return (
-            <div className="gotoTop">
-              <Button title='????' className='scroll' 
-                      onClick={ () => { this.scrollToTop(); }}>
-                <Icon icon={chevronUp} className="arrow-up"/>
-              </Button>
-            </div>
+
+
+      <div title="回到顶部">
+        <ScrollUpButton ContainerClassName="gotoTop" style={{'bottom': '60px'}}/>
+      </div>
+
+            
         );
     }
 } 
