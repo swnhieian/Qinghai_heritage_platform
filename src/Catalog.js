4ba6e67408ba38ec08de013407d42a919af06118
Shi Weinan <swnhieian@126.com>
2018-08-07 20:16:56 +0800

    fix catalog scroll problem

--- src/Catalog.js
+++ src/Catalog.js
@@ -71,15 +71,53 @@ function ContentPane(props) {
     );
 }
 
+//将URL中的UTF-8字符串转成中文字符串
+function getCharFromUtf8(str) {
+    var cstr = "";
+    var nOffset = 0;
+    if (str == "")
+        return "";
+    str = str.toLowerCase();
+    nOffset = str.indexOf("%e");
+    if (nOffset == -1)
+        return str;
+    while (nOffset != -1) {
+        cstr += str.substr(0, nOffset);
+        str = str.substr(nOffset, str.length - nOffset);
+        if (str == "" || str.length < 9)
+            return cstr;
+        cstr += utf8ToChar(str.substr(0, 9));
+        str = str.substr(9, str.length - 9);
+        nOffset = str.indexOf("%e");
+    }
+    return cstr + str;
+}
+
+//将编码转换成字符
+function utf8ToChar(str) {
+    var iCode, iCode1, iCode2;
+    iCode = parseInt("0x" + str.substr(1, 2));
+    iCode1 = parseInt("0x" + str.substr(4, 2));
+    iCode2 = parseInt("0x" + str.substr(7, 2));
+    return String.fromCharCode(((iCode & 0x0F) << 12) | ((iCode1 & 0x3F) << 6) | (iCode2 & 0x3F));
+}
+
+
+
+
 class Catalog extends Component {
     constructor(props) {
         super(props);
-        this.state = {sidebarData: []};
+        
+
+
+        this.state = {sidebarData: [], location: props.location};
     }
 
     componentDidMount() {
         this.loadData();
         Scroll.scrollSpy.update();
+        
     }
 
     loadData() {
@@ -87,6 +125,15 @@ class Catalog extends Component {
             if (response.ok) {
                 response.json().then(data => {
                     this.setState({sidebarData: data.categories});
+                    if (this.state.location.hash) {
+                        Scroll.scroller.scrollTo(getCharFromUtf8(this.state.location.hash.slice(1)), {
+                            duration: 500,
+                            smooth: true,
+                            spy: true,
+                            hashSpy: true,
+                            offset: -50 // Scrolls to element - 50 pixels down the page
+                          });
+                    }
                 });
             } else {
                 response.json().then(error => {
--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ import CarouselSlides from "./CarouselSlides";
 
 const Card = (props) => (
     <Col key={props.idx}>
-      <Link to="/catalog" className="catalog-card">
+      <Link to={"/catalog#"+props.item.category} className="catalog-card">
         <img src={props.item.thumbnail} alt={props.item.thumbnail} className="rounded"/>
         <div className="catalog-name d-flex align-items-center justify-content-around">
           <span className="d-flex">{props.item.category}</span>
@@ -67,12 +67,12 @@ class Home extends Component {
               <CarouselSlides/>
               <Catalogs catalogs={this.state.catalogs}/>
               <div className="tangka "> 
-                <div class="jumbotron">
+                <div className="jumbotron">
                   <Container>
-                    <h1 class="display-4">唐卡</h1>
-                    <p class="lead">也叫唐嘎，唐喀，系藏文音译，指用彩缎装裱后悬挂供奉的宗教卷轴画。唐卡是藏族文化中一种独具特色的绘画艺术形式，题材内容涉及藏族的历史、政治、文化和社会生活等诸多领域，传世唐卡大都是藏传佛教和本教作品。</p>
-                    <hr class="my-4" /> <p>青海非物质文化博物馆的宗旨：让所有青海非物质文化遗产三维化，让人可以通过视频、音频、AR、VR等方式了解和传承青海非物质文化遗产。</p>
-                    <p class="lead">
+                    <h1 className="display-4">唐卡</h1>
+                    <p className="lead">也叫唐嘎，唐喀，系藏文音译，指用彩缎装裱后悬挂供奉的宗教卷轴画。唐卡是藏族文化中一种独具特色的绘画艺术形式，题材内容涉及藏族的历史、政治、文化和社会生活等诸多领域，传世唐卡大都是藏传佛教和本教作品。</p>
+                    <hr className="my-4" /> <p>青海非物质文化博物馆的宗旨：让所有青海非物质文化遗产三维化，让人可以通过视频、音频、AR、VR等方式了解和传承青海非物质文化遗产。</p>
+                    <p className="lead">
                       <a href="http://192.168.1.102:8080/web3dproduct/ArtAndCraft/tangkaindex" target="_blank"><button className="btn btn-outline-light btn-lg">中国唐卡图像数字博物馆</button></a>
                   </p></Container>
                 </div>
